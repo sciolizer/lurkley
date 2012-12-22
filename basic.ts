abebb7586912dda6b1e8c994bcfe54f1df5b8568
Joshua Ball <JoshBBall@gmail.com>
2012-12-22 14:43:30 -0800

    everything stubbed out, maybe

--- basic.ts
+++ basic.ts
@@ -1,41 +1,57 @@
 var trs80 = (function() {
   var next = null;
   var quit = false;
+  var pg = null;
   var bs = {
     _next: function(n) { if (next == null) { next = n; } },
     _quit: function() { quit = true; },
-    assign: null,
-    assignArr: null,
-    circle: null,
+    assign: function() { console.log("assign"); },
+    assignArr: function() { console.log("assignArr"); },
+    circle: function() { console.log("circle"); },
     clear: function(n) { console.log("clear"); },
-    color: null,
-    dim: null,
-    draw: null,
-    for: null,
-    get: null,
-    getTo: null,
-    gosub: null,
-    goto: null,
-    line: null,
-    lineTo: null,
-    next: null,
-    onGoto: null,
-    onGosub: null,
-    paint: null,
-    pcls: null,
-    play: null,
-    pmode: null,
-    poke: null,
-    pset: null,
-    put: null,
-    putTo: null,
-    return: null,
-    screen: null,
-    sound: null,
-    recall: null,
-    recallArr: null,
-    inkey: null,
-    len: null
+    color: function() { console.log("color"); },
+    dim: function() { console.log("dim"); },
+    draw: function() { console.log("draw"); },
+    for: function() { console.log("for"); },
+    get: function() { console.log("get"); },
+    getTo: function() { console.log("getTo"); },
+    gosub: function() { console.log("gosub"); },
+    goto: function(i) { next = pg["line" + i + "_0"]; },
+    line: function() { console.log("line"); },
+    lineTo: function() { console.log("lineTo"); },
+    next: function() { console.log("next"); },
+    onGoto: function() { console.log("onGoto"); },
+    onGosub: function() { console.log("onGosub"); },
+    paint: function() { console.log("paint"); },
+    pcls: function() { console.log("pcls"); },
+    play: function() { console.log("play"); },
+    pmode: function() { console.log("pmode"); },
+    poke: function() { console.log("poke"); },
+    pset: function() { console.log("pset"); },
+    put: function() { console.log("put"); },
+    putTo: function() { console.log("putTo"); },
+    return: function() { console.log("return"); },
+    screen: function() { console.log("screen"); },
+    sound: function() { console.log("sound"); },
+    recall: function() { console.log("recall"); },
+    recallArr: function() { console.log("recallArr"); },
+    len: function() { console.log("len"); },
+    isEqual: function() { console.log("isEqual"); },
+    isUnequal: function() { console.log("isUnequal"); },
+    isLessThan: function() { console.log("isLessThan"); },
+    isLessThanOrEqual: function() { console.log("isLessThanOrEqual"); },
+    isGreaterThan: function() { console.log("isGreaterThan"); },
+    isGreaterThanOrEqual: function() { console.log("isGreaterThanOrEqual"); },
+    inkey: function() { console.log("inkey"); },
+    chr: function() { console.log("chr"); },
+    instr: function() { console.log("instr"); },
+    int: function() { console.log("int"); },
+    left: function() { console.log("left"); },
+    mid: function() { console.log("mid"); },
+    rnd: function() { console.log("rnd"); },
+    str: function() { console.log("str"); },
+    string: function() { console.log("string"); },
+    val: function() { console.log("val"); },
   };
   var step = function() {
     if (next == null) {
@@ -48,10 +64,11 @@ var trs80 = (function() {
     if (quit) {
       console.log("done");
     } else {
-      setTimeout(step, 1000);
+      setTimeout(step, 100);
     }
   };
   var run = function(pr, st) {
+    pg = pr;
     next = st;
     setTimeout(step, 100);
   };
