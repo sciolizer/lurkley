36ea95df6521f1148e42f6cc3d46e6aaec535b6b
Joshua Ball <JoshBBall@gmail.com>
2012-12-22 17:20:15 -0800

    cocoColor

--- basic.ts
+++ basic.ts
@@ -1,3 +1,4 @@
+declare var processingQueue;
 var trs80 = (function() {
   var next = null;
   var quit = false;
@@ -12,6 +13,32 @@ var trs80 = (function() {
   var stack = [];
   var onNext = null;
   var last = function(str) { return str.substring(str.length - 1); }
+  var process = function(f) {
+    processingQueue.unshift(f);
+  };
+  var cocoColor = function(i) {
+    switch (i) {
+      case 0: // black
+        return [0, 0, 0];
+      case 1: // green
+        return [0, 128, 0];
+      case 2: // yellow
+        return [255, 255, 0];
+      case 3: // blue
+        return [0, 0, 255];
+      case 4: // red
+        return [255, 0, 0];
+      case 5: // white
+        return [255, 255, 255];
+      case 6: // cyan
+        return [0, 255, 255];
+      case 7: // magenta
+        return [255, 0, 255];
+      case 8: // orange
+        return [255, 165, 0];
+    }
+    throw ("unrecognized color: " + i);
+  };
   var bs = {
     _next: function(n) {
       if (onNext != null) {
@@ -44,7 +71,7 @@ var trs80 = (function() {
       }
     },
     circle: function(x, y, rad, a1, a2, a3, a4) { console.log("circle"); },
-    clear: function(n) { console.log("clear"); },
+    clear: function(n) { ; /* no-op; os handles memory management */ },
     color: function(a1, a2) { console.log("color"); },
     dim: function(arrName, sizes) {
       if (arrName == "A") {
